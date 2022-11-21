bf70f81ee9acd68c87e7c86843288aee206568e6
m-kanemaru <m.kanemaru.1059@gmail.com>
2022-11-21 11:45:45 +0000

    通知修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,28 +6,28 @@ class Post < ApplicationRecord
     has_many :post_comments, dependent: :destroy
     has_many :tags, dependent: :destroy
     
-    # def create_notification_comment!(current_user, post_comment_id)
-    #     temp_ids = PostComment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
-    #     temp_ids.each do |temp_id|
-    #         save_notification_comment!(current_user, post_comment_id, temp_id['user_id'])
-    #     end
-    #     save_notification_comment!(current_user, post_comment_id, user_id) if temp_ids.blank?
-    # end
+    def create_notification_comment!(current_user, post_comment_id)
+        temp_ids = PostComment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
+        temp_ids.each do |temp_id|
+            save_notification_comment!(current_user, post_comment_id, temp_id['user_id'])
+        end
+        save_notification_comment!(current_user, post_comment_id, user_id) if temp_ids.blank?
+    end
 
-    # def save_notification_comment!(current_user, post_comment_id, visited_id)
-    #     notification = current_user.active_notifications.new(
-    #         post_id: id,
-    #         post_comment_id: post_comment_id,
-    #         visited_id: visited_id,
-    #         action: 'comment'
-    #     )
-    #     if notification.visiter_id == notification.visited_id
-    #         notification.checked = true
-    #     end
-    #     notification.save
-    #     if notification.valid?
-    #     end
-    # end
+    def save_notification_comment!(current_user, post_comment_id, visited_id)
+        notification = current_user.active_notifications.new(
+            post_id: id,
+            post_comment_id: post_comment_id,
+            visited_id: visited_id,
+            action: 'comment'
+        )
+        if notification.visiter_id == notification.visited_id
+            notification.checked = true
+        end
+        notification.save
+        if notification.valid?
+        end
+    end
     
      has_one_attached :image_id
     
